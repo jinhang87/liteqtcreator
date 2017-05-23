8c4e7c7483c63d7c79e73c8648945f2f15ddffcc
jinhang <jinhang87@163.com>
2017-05-23 17:20:33 +0800

    [add]  deletemakefiles.pl

--- script/deletemakefiles.pl
+++ script/deletemakefiles.pl
@@ -5,16 +5,14 @@ use strict;
 use Cwd;
 
 my $cwd = getcwd;
-print "$cwd\n" ;
 my $path = $cwd.'/../';
-print "$path\n" ;
 
 sub wanted {
     if ( -f $File::Find::name ) {
-        #if ( $File::Find::name =~ /\Makefile$/ ) {
+        if ( $File::Find::name =~ /\Makefile$/ ) {
             print "Delete File: $File::Find::name\n";
-		#	unlink($File::Find::name) or warn "failed on $File::Find::name";
-        #}
+            unlink($File::Find::name) or warn "failed on $File::Find::name";
+        }
     }
 }
 
