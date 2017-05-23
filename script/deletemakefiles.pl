55ad76b8d802a97589ca9e7c59fbeda51796351c
jinhang <jinhang87@163.com>
2017-05-23 18:02:26 +0800

    [chg] deletemakefiles.pl

--- script/deletemakefiles.pl
+++ script/deletemakefiles.pl
@@ -2,14 +2,31 @@
 use warnings;
 use File::Find;
 use strict;
-use Cwd;
+use File::Basename;
+use FindBin qw($Bin);
 
-my $cwd = getcwd;
+my $cwd = $Bin;
 my $path = $cwd.'/../';
+print "find path: $path\n";
+
+sub wanted_print {
+	if ( -d $File::Find::dir ){
+		#print "Find Dir: $File::Find::dir\n";
+	}
+    if ( -f $File::Find::name ) {
+		#print "Find File: $File::Find::name\n";
+    }
+}
 
 sub wanted {
+	if ( -d $File::Find::dir ){
+		if ( $File::Find::dir =~ /\.git/ ){
+			#print "Delete Dir: $File::Find::dir\n";
+			return;
+		}
+	}
     if ( -f $File::Find::name ) {
-        if ( $File::Find::name =~ /\Makefile$/ ) {
+        if ( $File::Find::name =~ /Makefile/ ) {
             print "Delete File: $File::Find::name\n";
             unlink($File::Find::name) or warn "failed on $File::Find::name";
         }
