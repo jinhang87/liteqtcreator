1b9bc977cc9afaf8367f9786cac52aad0c0c9b40
jinhang <jinhang87@163.com>
2017-07-17 16:31:48 +0800

    []add

---
+++ src/plugins/subplugin/ZH_CN.ts
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="zh_CN">
+<context>
+    <name>ioMenuIconFactory</name>
+    <message>
+        <location filename="iomenuiconfactory.cpp" line="+32"/>
+        <source>button</source>
+        <translatorcomment>按钮啊啊啊啊</translatorcomment>
+        <translation>按钮啊啊啊啊</translation>
+    </message>
+    <message>
+        <location line="+1"/>
+        <source>label</source>
+        <translatorcomment>按钮啊啊啊啊</translatorcomment>
+        <translation>按钮啊啊啊啊</translation>
+    </message>
+</context>
+</TS>
